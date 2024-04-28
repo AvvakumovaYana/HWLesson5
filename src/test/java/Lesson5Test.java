f397d46bb2c79b1d12d0e26d992f34ea6271fbeb
Yana Avvakumova <10.07.93t@gmail.com>
2024-04-28 20:46:16 +0300

    HWLesson5 was changed!

--- src/test/java/Lesson5Test.java
+++ src/test/java/Lesson5Test.java
@@ -1,12 +1,13 @@
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.DragAndDropOptions.to;
 import static com.codeborne.selenide.Selectors.byTagAndText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class Lesson5Test {
     @Test
-    void GithubTest (){
+    void githubTest (){
         open("https://github.com/");
         $(byTagAndText("button","Solutions")).hover();
         $(byTagAndText("a","Enterprise")).click();
@@ -14,9 +15,18 @@ public class Lesson5Test {
     }
 
     @Test
-    void DragDropTest (){
+    void dragDropTest (){
         open("https://the-internet.herokuapp.com/drag_and_drop");
         actions().moveToElement($("#column-a")).clickAndHold().moveByOffset(200, 0).release().perform();
         $("#column-a").shouldHave(text("b"));
+        $("#column-b").shouldHave(text("a"));
+    }
+
+    @Test
+    void dragDrop2Test (){
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDrop(to("#column-b"));
+        $("#column-a").shouldHave(text("b"));
+        $("#column-b").shouldHave(text("a"));
     }
 }
